ad088e6f5e5d0961201d52c753e607519f5dedfe
tbeagle2 <jonathan4812@gmail.com>
2023-10-13 16:59:09 -0700

    refactoring with updated code/adding functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,113 +1,120 @@
-const operators = ['( )', '%', '÷', 'x', '-', '+']
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
-const alterers = ['AC', "=", '&lt;-']
-const topBar = ['√ ', 'π', '^', '!']
+// to do
+// re-do how I select values on the calculator
+// error catching
+// reconnect the script the to html.
 
-// add history
-// add pulldown that shows RAD sin cos tan INV e In log. Put those values into an array
-// add pulldown to show history
-// add dark and light mode, after stylings done. Can be a symbol in the top right where the hamburger menu is
-// css minimum sizes and max sizes, black background?
-const numpad = document.querySelector('.numpad')
+
 const input = document.getElementById("input-field");
-input.textContent = ""
-let inputField = []
-let inputValues = [] // this is the list of what's been selected and is on screen
-let calculation = []
-let history = []
-let duplicateEquals = []
-const answerField = document.getElementById('answer')
-let calculatedContent = 5
-answerField.textContent = calculatedContent
-// turn the operator and alterer events into their own function, so when x or equal is pressed
-// what will happen will be in these functions.
+const answer = document.getElementById("answer")
+
+const operators = ['(', ')', '%', '÷', 'x', '-', '+']
 
 
-
-function buttonPressed(event) {
-
-    // Gets number pressed
-    for (let num = 0; num < 10; num++) {
-        if (event.target.textContent == num) {
-            inputField.push(num)
-        }
-    }
-
-    //gets operator pressed
-    for (let op = 0; op < operators.length; op++) {
-        if (event.target.textContent == operators[op]) {
-            let operator = operators[op]
-            if (operator == "( )" && inputField.slice(-1) == "(") {
-                inputField.push(")")
-            } else if (inputField.slice(-1) == ")") {
-                inputField.push(")")
-            } else if (operator == "( )"){
-                inputField.push("(")
-            }
-            
-            // checks if an operator was entered last.
-            if (operator == "(" || operator == ")") {
-                break;
-            }
-            if (inputField.length > 0 && inputField.slice(-1).includes(operator)) {
-                console.log("error")
-            } else {
-                inputField.push(operator)
-                inputValues.push(parseInt(input.textContent))
-                inputValues.push(operator)
-            }
-            }
-    }
-    //gets alterers pressed
-    for (let alt = 0; alt < alterers.length; alt++) {
-        if (event.target.textContent == alterers[alt]) {
-            let alterer = alterers[alt]
-            inputField.push(alterer)
-        } if (event.target.textContent == 'AC') {
-            input.textContent = ''
-        }
-    }
-    // getting values to put on the screen
-    input.textContent = null;
-    for (let item = 0; item < inputField.length; item++) {
-        let value = inputField[item].toString()
-        input.textContent = input.textContent + value; 
-    }
-    console.log(input.textContent)
-    // console.log(calculation)
-    // console.log(inputField)
-
-    // calculating if equals is pushed
-    if (event.target.textContent == "=") {
-        let removeEquals = inputField.slice(0, -1) // removes equals
-        // let removeEquals = inputField.replace('=', '')
-        let joinedInput = removeEquals.join(''); //combines array with no separator
-        console.log(removeEquals);
-        console.log(eval(joinedInput));
-        duplicateEquals.push(eval(joinedInput))
-        // input.textContent = joinedInput.replace('=', '');
-        // console.log(input.textContent)
-        // console.log(eval(removeEquals))
-        // console.log(joinedInput)
-        //replace text in input box
-        console.log(inputValues)
-    }
-
+function setDisplay(clicked) {
+    input.textContent += clicked;
 }
 
-// function History() {
-    
-// }
+// checks the input to see if an open parentheses was added or not.
+// check if input has parenthesis and if it does, which one was last.
+// then checks if the opening parenthese is after or before the closing, and if it's after, and there's 
+// a number as the last item in the string, it will add a closing parenthesis, else, it will add an opening
 
-// need to remove equals from inputField, then have a second value that is displayed under
-// the input field
+function setParenthese() {
+    let inputValue = input.textContent 
+    if (!inputValue) {
+        setDisplay("(")
+    } else if (inputValue.lastIndexOf("(") > inputValue.lastIndexOf(")") && inputValue.charAt(inputValue.length-1) == !isNaN(inputValue.charAt(inputValue.length-1))) {
+        setDisplay(")")
+    } else {
+        setDisplay("(")
+    }
+}
 
-// font size decreases 8 times.
-// after 9, start decreasing the font. Can do a for loop that checks the amount of 
-// of characters. Maybe using a forloop if characters is more than 9, then another if for loop
-// that cycles through text sizes. So first have an array of sizes, than an if check if the characters
-// are more than 9, then a for loop inside of that that will check the font size, and then multiple if
-// statements that will check both the size and character ammount to reduce it.
+function answerCalculated() {
+    let displayedContent = input.textContent
+    let joinedInput = displayedContent.join('')
+    input.textContent = (eval(joinedInput)).toString();
+    let calculatedDisplay = number(input.textContent);
+    return calculatedDisplay
+}
 
-numpad.addEventListener('click', buttonPressed);
+// JavaScript has built in Math functions.
+// couldn't figure out how the android calculator app had the sqrRoot symbol in the display
+// and could still update/get a value when adding more numbers to the input 
+function squareRoot() {
+    displayedContent = input.textContent
+    if (!displayedContent) {
+        input.textContent = "NaN";
+    } else {
+        answer.textContent = eval(Math.sqrt(displayedContent))
+    }
+}
 
+function pie() {
+    displayedContent = input.textContent
+    if (!displayedContent) {
+        input.textContent = "3.141592653589793"
+    } else {
+    eval(Math.sqrt(displayedContent))
+    }
+}
+
+// exponent is ** and can just work by itself. so if ^ is pressed, add ** to contents
+
+function factorial() {
+    if (!input.textContent) {
+        return input.textContent = "Format error"
+    } else {
+        num = input.textContent;
+    }
+    // the above checks if the string is empty, then it will break the function, since 0 is considered
+    // falsy as well and 0 is a possible value of a factorial
+    let num = answerCalculated(); // calculates the current data in the input so we can 
+    // calculate the factorial based on the sum and not have any errors if there is more than
+    // just numbers in the data.    
+    let answer = 1;
+    if (num == 0 || num == 1){
+      return answer;
+    }
+    else if(num > 1){
+      for(let i = num; i >= 1; i--){
+        answer = answer * i;
+      }
+      return answer;
+    }
+    else{
+      return input.textContent = "Format error"
+    }  
+}
+// get the display value, and then multiple that by sin, and have an entered number for sin
+function sin(num) {
+    return answer.textContent = Math.sin(num)
+}
+// need to get the input value and add the sin, cos, tan, to a list so we can close the values inside the
+// parenthesis
+function cos() {
+    answer.textContent = Math.cos(num)
+}
+
+function tan() {
+    answer.textContent = Math.tan(num)
+}
+
+// e stands for exponent of 10, but I don't like single character functions or variables
+function eten() {
+    // calculated answer**10
+    return answer.textContent = answerCalculated()**10 
+}
+// need to figure out how to keep updating the answer when inputing numbers after e is click
+// for below, repeat the calculations if one of these is clicked. Maybe have a truth or false variable inside
+// the calculation function that will check if these buttons are clicked.
+
+//natural logarithm. If numbers before it, then it will be multiplied by those numbers
+function naturalLog(num) {
+    answer.textContent = Math.log(num) * answerCalculated()
+}
+
+// logarithm with a base of ten
+function alog() {
+    answer.textContent = Math.log10(num) * answerCalculated()
+}
--- scripts/functions.js
+++
@@ -1,3 +0,0 @@
-function getSelected(event) {
-    
-}
